baae3accb10629c9c33f94652865f1cc369fa238
samad13 <61961655+samad13@users.noreply.github.com>
2020-04-19 21:17:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function arr(1,2,3,4.5){
+    console.log(1 + 2 + 3 + 4 + 5);
+  }
+  const data[]
+for(let i=0; i/1; i++){
+    if(i%2===0){
+        data.push(i);
+    }
+   Console.log(data) ;
+} 
+ let arr = [0, 1, 2, 3, 4, 5]
+if(arr/2) {
+    console.log('yu')
+}
+else if(arr/3){
+   console.log ('gi')
+}
+else if(arr/5){
+    console.log('oh')
+
+}
+
+  
